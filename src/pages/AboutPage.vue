80c0ea993627aef48da35c9b6130f3f3001040f7
LakshithaRJ <lakshrj139@gmail.com>
2024-06-20 12:24:20 -0500

    add content to About page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,27 @@
 <template>
-    <h5>About</h5>
+    <q-page class="q-pa-lg">
+        <h4 class="q-mt-none q-mb-md text-weight-bold">About Twitter</h4>
+        <div class="text-body1">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iure voluptates autem cum eum sequi
+                iusto
+                deserunt. Distinctio et nesciunt dolores nisi eligendi! Nihil labore, explicabo repellendus quos dolorum
+                quis.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iure voluptates autem cum eum sequi
+                iusto
+                deserunt. Distinctio et nesciunt dolores nisi eligendi! Nihil labore, explicabo repellendus quos dolorum
+                quis.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore iure voluptates autem cum eum sequi
+                iusto
+                deserunt. Distinctio et nesciunt dolores nisi eligendi! Nihil labore, explicabo repellendus quos dolorum
+                quis.
+            </p>
+        </div>
+    </q-page>
 </template>
 <script>
 export default {
