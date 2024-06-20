3c979812dbb20b025f9c18e5a9888f62363fbf9b
LakshithaRJ <lakshrj139@gmail.com>
2024-06-20 13:14:41 -0500

    load tweets on feed and add tweet actions

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -18,6 +18,38 @@
     </div>
 
     <q-separator size="10px" color="grey-2" class="divider" />
+
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"> <strong> Laksh J</strong> <span class="text-grey-7">@Laksh-j</span>
+          </q-item-label>
+          <q-item-label class="tweet-content text-body1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quisquam nam aspernatur sed exercitationem
+            reprehenderit tempore error laboriosam dolores.
+
+            Nihil facilis exercitationem minima aperiam praesentium
+            quibusdam ipsum ipsam veniam sapiente?
+          </q-item-label>
+          <div class="row justify-between q-mt-sm tweet-icons">
+            <q-btn round flat color="grey" icon="far fa-comment" size="sm" />
+            <q-btn round flat color="grey" icon="fas fa-retweet" size="sm" />
+            <q-btn round flat color="grey" icon="far fa-heart" size="sm" />
+            <q-btn round flat color="grey" icon="fas fa-trash" size="sm" />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top>
+          1 min ago
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -40,4 +72,8 @@ defineOptions({
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-3
+.tweet-content
+  white-space: pre-line
+.tweet-icons
+  margin-left: -5px
 </style>
